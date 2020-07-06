4f9bbfd6a9ee4fe4c8ba54b6ea6963e6ab01d64b
Sushant Kumar <Sushant.kumar@QUALITEST.LOCAL>
2020-07-06 12:29:20 +0100

    Add tests for home page

--- cypress/support/PageObjectsMap/HomePage.js
+++ cypress/support/PageObjectsMap/HomePage.js
@@ -1,5 +1,5 @@
 class HomePage {
-	getCovid() {
+	getCovidMessage() {
 		return cy.get(".covid");
 	}
 	getCovidLink() {
@@ -10,6 +10,34 @@ class HomePage {
 	}
 	getHomeHyperLink() {
 		return cy.get(":nth-child(1) > ul > :nth-child(3) > a");
+	}
+	getMailingStripText() {
+		return cy.get(".home-hero__mailing-strip__text");
+	}
+	getMailingStripButton() {
+		return cy.get(".home-hero__mailing-strip__button__inner > p");
+	}
+	getMyStoryInToTeaching() {
+		return cy.get("div.my-story__right p:nth-child(3) > a.git-link");
+	}
+
+	getHomeBannerText() {
+		return cy.get(".home-hero__banner >h1");
+	}
+	getBannerText() {
+		return cy.get(".hero__banner__text > h1");
+	}
+
+	getContentVideo() {
+		return cy.get(
+			'[href="https://www.youtube.com/watch?v=MLdrZJpK5rU"] > .content-video > .content-video__play > .icon-play'
+		);
+	}
+	getVideoContainer() {
+		return cy.get(".video-overlay__video-container");
+	}
+	getVideoCloseIcon() {
+		return cy.get(".icon-video-close");
 	}
 }
 export default HomePage;
