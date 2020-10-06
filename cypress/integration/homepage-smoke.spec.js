a88abb435dad805fdd59b7c8ee84119515b949af
Craig Taylor <craig.taylor@sevenwhitepixels.co.uk>
2020-10-06 12:41:55 +0100

    reference navlinks

--- cypress/integration/homepage-smoke.spec.js
+++ cypress/integration/homepage-smoke.spec.js
@@ -1,5 +1,6 @@
 /// <reference types="Cypress" />
 import Homepage from "../support/pageobjects/Homepage";
+import Navlinks from "../support/pageobjects/Navlinks";
 
 function terminalLog(violations) {
 	cy.task(
@@ -38,10 +39,10 @@ describe(
 		it('Links through to "Funding your training"', () => {
 			homePage.getFundingyourTrainingLink().then(function (linkText) {
 				cy.contains(linkText.text()).should((el) => {
-					expect(el).to.have.attr("href", "/funding-your-training");
+					expect(el).to.have.attr("href", Navlinks.fundingYourTraining);
 				}).click();
 			});
-			cy.location("pathname").should("equal", "/funding-your-training");
+			cy.location("pathname").should("equal", Navlinks.fundingYourTraining);
 			cy.shouldHaveTalkToUsSection();
 			cy.shouldHaveFooter();
 		});
@@ -50,11 +51,11 @@ describe(
 			homePage.getStepstoBecomeTeacherLink().then(function (linkText) {
 				cy.contains(linkText.text())
 					.should((el) => {
-						expect(el).to.have.attr("href", "/steps-to-become-a-teacher");
+						expect(el).to.have.attr("href", Navlinks.stepsToBecomeATeacher);
 					})
 					.click();
 			});
-			cy.location("pathname").should("equal", "/steps-to-become-a-teacher");
+			cy.location("pathname").should("equal", Navlinks.stepsToBecomeATeacher);
 			cy.shouldHaveTalkToUsSection();
 			cy.shouldHaveFooter();
 		});
@@ -63,11 +64,11 @@ describe(
 			homePage.getTeachingAsaCareerLink().then(function (linkText) {
 				cy.contains(linkText.text())
 					.should((el) => {
-						expect(el).to.have.attr("href", "/life-as-a-teacher");
+						expect(el).to.have.attr("href", Navlinks.lifeAsATeacher);
 					})
 					.click();
 			});
-			cy.location("pathname").should("equal", "/life-as-a-teacher");
+			cy.location("pathname").should("equal", Navlinks.lifeAsATeacher);
 			cy.shouldHaveTalkToUsSection();
 			cy.shouldHaveFooter();
 		});
@@ -76,16 +77,13 @@ describe(
 			homePage.getSalariesAndBenefitsLink().then(function (linkText) {
 				cy.contains(linkText.text())
 					.should((el) => {
-						expect(el).to.have.attr(
-							"href",
-							"/life-as-a-teacher/teachers-salaries-and-benefits"
-						);
+						expect(el).to.have.attr("href",Navlinks.salariesAndBenefits);
 					})
 					.click();
 			});
 			cy.location("pathname").should(
 				"equal",
-				"/life-as-a-teacher/teachers-salaries-and-benefits"
+				Navlinks.salariesAndBenefits
 			);
 			cy.shouldHaveTalkToUsSection();
 			cy.shouldHaveFooter();
@@ -95,11 +93,11 @@ describe(
 			homePage.getFindanEventNearYouLink().then(function (linkText) {
 				cy.contains(linkText.text())
 					.should((el) => {
-						expect(el).to.have.attr("href", "/events");
+						expect(el).to.have.attr("href", Navlinks.events);
 					})
 					.click();
 			});
-			cy.location("pathname").should("equal", "/events");
+			cy.location("pathname").should("equal", Navlinks.events);
 			cy.shouldHaveTalkToUsSection();
 			cy.shouldHaveFooter();
 		});
@@ -108,24 +106,24 @@ describe(
 			cy.contains("Please check here for updates").then(function (linkText) {
 				cy.contains(linkText.text())
 					.should((el) => {
-						expect(el).to.have.attr("href", "/covid-19");
+						expect(el).to.have.attr("href", Navlinks.covid19);
 					})
 					.click();
 			});
-			cy.location("pathname").should("equal", "/covid-19");
+			cy.location("pathname").should("equal", Navlinks.covid19);
 			cy.shouldHaveTalkToUsSection();
 			cy.shouldHaveFooter();
 		});
 
 		it('Links through to "Sign up here"', () => {
 			homePage.getMailingStripButton().dblclick();
-			cy.location("pathname").should("equal", "/mailinglist/signup");
+			cy.location("pathname").should("equal", Navlinks.mailingListSignup);
 		});
 		it('Links through to "My story into teaching"', () => {
 			homePage.getMyStoryInToTeaching().click();
 			cy.location("pathname").should(
 				"equal",
-				"/life-as-a-teacher/my-story-into-teaching"
+				Navlinks.myStoryIntoTeaching
 			);
 			cy.shouldHaveTalkToUsSection();
 			cy.shouldHaveFooter();
@@ -135,7 +133,7 @@ describe(
 			cy.get(":nth-child(3) > .content__left > .call-to-action-button").click();
 			cy.location("pathname").should(
 				"equal",
-				"/life-as-a-teacher/my-story-into-teaching/career-changers"
+				Navlinks.careerChangers
 			);
 		});
 
@@ -144,7 +142,7 @@ describe(
 			cy.get(":nth-child(6) > .content__left > .call-to-action-button").click();
 			cy.location("pathname").should(
 				"equal",
-				"/life-as-a-teacher/my-story-into-teaching/international-career-changers"
+				Navlinks.internationalCareerChangers
 			);
 		});
 
@@ -153,7 +151,7 @@ describe(
 			cy.get(":nth-child(9) > .content__left > .call-to-action-button").click();
 			cy.location("pathname").should(
 				"equal",
-				"/life-as-a-teacher/my-story-into-teaching/teacher-training-stories"
+				Navlinks.teacherTrainingStories
 			);
 		});
 		it('Links through to "making-a-difference"', () => {
@@ -163,7 +161,7 @@ describe(
 			).click();
 			cy.location("pathname").should(
 				"equal",
-				"/life-as-a-teacher/my-story-into-teaching/making-a-difference"
+				Navlinks.makingADifference
 			);
 		});
 
@@ -174,7 +172,7 @@ describe(
 			).click();
 			cy.location("pathname").should(
 				"equal",
-				"/life-as-a-teacher/my-story-into-teaching/career-progression"
+				Navlinks.careerProgression
 			);
 		});
 		it('Links through to "returning to teaching"', () => {
@@ -184,26 +182,27 @@ describe(
 			).click();
 			cy.location("pathname").should(
 				"equal",
-				"/life-as-a-teacher/my-story-into-teaching/returners"
+				Navlinks.returners
 			);
 		});
 
 		it('Links through to "Find events"', () => {
 			homePage.getFindEventLink().click();
-			cy.location("pathname").should("equal", "/events");
+			cy.location("pathname").should("equal", Navlinks.events);
 			homePage.getBannerText().should("exist");
 			cy.shouldHavePageNavigation();
 			cy.shouldHaveTalkToUsSection();
 			cy.shouldHaveFooter();
 		});
-		/*
-	Below test meets the following creteria -
-	 1. It should only open/expand the Check your qualifications link. 
-	 2. Other than this, all link should be closed/not expanded. 
+
+	/*
+	Below test meets the following criteria -
+		1. It should only open/expand the Check your qualifications link. 
+		2. Other than this, all link should be closed/not expanded. 
 	*/
 		it('Links through to "Check your qualifications"', () => {
 			homePage.getCheckYourQualificationsLink().click();
-			cy.location("pathname").should("equal", "/steps-to-become-a-teacher");
+			cy.location("pathname").should("equal", Navlinks.stepsToBecomeATeacher);
 			homePage.getBannerText().should("exist");
 			//Below property has been removed
 			/*cy.get("#collapsable-icon-1").should(
@@ -230,7 +229,7 @@ describe(
 
 		it('Links through to "Ways to train"', () => {
 			homePage.getWaystoTrainLink().siblings().click();
-			cy.location("pathname").should("equal", "/steps-to-become-a-teacher");
+			cy.location("pathname").should("equal", Navlinks.stepsToBecomeATeacher);
 			homePage.getBannerText().should("exist");
 			//Below property has been removed
 			/*cy.get("#collapsable-icon-1").should(
