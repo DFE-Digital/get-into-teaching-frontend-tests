30e03bb7c85adc3b7d2d3742176d66f6ed4c9946
Sushant Kumar <Sushant.kumar@QUALITEST.LOCAL>
2020-11-25 16:21:40 +0000

    Update mailinglist specs

--- cypress/integration/mailinglist-signup-flow.spec.js
+++ cypress/integration/mailinglist-signup-flow.spec.js
@@ -242,6 +242,85 @@ describe(`Feature - Mailing list sign up : Tests execution date and time : ${new
 	});
 
 	it("Links through to feedback survey page", function () {
+		let rnum = Math.floor(Math.random() * 1000000000 + 1);
+		let email = `testuser${rnum.toString()}@mail.co.uk`;
+		cy.enterFirstNameLastNameAndEmailAddress(
+			this.testInputData.firstName,
+			this.testInputData.lastName,
+			email
+		);
+		mailingListSignUp.getNextStep().click();
+		cy.degreeStage("Yes, I already have a degree");
+		mailingListSignUp.getNextStep().click();
+		mailingListSignUp
+			.getStage()
+			.select(this.testInputData.howCloseAreYoutoApplyingForTeacherTraining);
+		mailingListSignUp.getNextStep().click();
+		mailingListSignUp.getSubjectToTeach().select(this.testInputData.whichSubjectdoYouWantToTeach);
+		mailingListSignUp.getNextStep().click();
+		mailingListSignUp.getPostcode().type(this.testInputData.postCode);
+		mailingListSignUp.getNextStep().click();
+		cy.acceptPrivacyPolicy();
+		mailingListSignUp.getCompleteSignUpButton().click();
+		mailingListSignUp.getContent().should("have.text", `You've signed up`);
+		cy.contains("Want to speak to us?")
+			.next()
+			.children()
+			.as("speakToUsSection")
+			.should("exist")
+			.invoke("attr", "href")
+			.then(function (targetLink) {
+				expect(targetLink).to.equal("tel:08003892501");
+			});
+		cy.get("@speakToUsSection").then(function (phoneNumber) {
+			cy.log();
+			expect(phoneNumber.text()).to.equal("0800 389 2501");
+		});
+		cy.contains("a", "feedback on this website").invoke("removeAttr", "target").click();
+		cy.get(".freebirdFormviewerViewHeaderTitle")
+			.should("exist")
+			.should("have.text", "Get into Teaching: Feedback Survey");
+	});
+
+	it('It hides the feedback bar if user clicks on "Hide" link', function () {
+		let rnum = Math.floor(Math.random() * 1000000000 + 1);
+		let email = `testuser${rnum.toString()}@mail.co.uk`;
+		cy.enterFirstNameLastNameAndEmailAddress(
+			this.testInputData.firstName,
+			this.testInputData.lastName,
+			email
+		);
+		mailingListSignUp.getNextStep().click();
+		cy.degreeStage("Yes, I already have a degree");
+		mailingListSignUp.getNextStep().click();
+		mailingListSignUp
+			.getStage()
+			.select(this.testInputData.howCloseAreYoutoApplyingForTeacherTraining);
+		mailingListSignUp.getNextStep().click();
+		mailingListSignUp.getSubjectToTeach().select(this.testInputData.whichSubjectdoYouWantToTeach);
+		mailingListSignUp.getNextStep().click();
+		mailingListSignUp.getPostcode().type(this.testInputData.postCode);
+		mailingListSignUp.getNextStep().click();
+		cy.acceptPrivacyPolicy();
+		mailingListSignUp.getCompleteSignUpButton().click();
+		mailingListSignUp.getContent().should("have.text", `You've signed up`);
+		cy.contains("Want to speak to us?")
+			.next()
+			.children()
+			.as("speakToUsSection")
+			.should("exist")
+			.invoke("attr", "href")
+			.then(function (targetLink) {
+				expect(targetLink).to.equal("tel:08003892501");
+			});
+		cy.get("@speakToUsSection").then(function (phoneNumber) {
+			cy.log();
+			expect(phoneNumber.text()).to.equal("0800 389 2501");
+		});
+		cy.get("#hide-feedback-bar").click();
+		cy.contains("a", "feedback on this website").should("not.be.visible");
+	});
+	/*it("Links through to feedback survey page", function () {
 		cy.enterFirstNameLastNameAndEmailAddress().then(() => {
 			mailingListSignUp.getNextStep().click();
 			cy.degreeStage("Yes, I already have a degree");
@@ -309,7 +388,7 @@ describe(`Feature - Mailing list sign up : Tests execution date and time : ${new
 			cy.get("#hide-feedback-bar").click();
 			cy.contains("a", "feedback on this website").should("not.be.visible");
 		});
-	});
+	});*/
 });
 
 describe(`Feature - 404 Not Found unknown_route : ${new Date()}`, () => {
